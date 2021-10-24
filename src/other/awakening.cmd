76dc8e1b820a52bf25c293cd32c276dfe4d6a74a
Curt Sellmer <sellmerfud@gmail.com>
2021-10-24 13:45:10 -0500

    Set version to 4.20

--- src/other/awakening.cmd
+++ src/other/awakening.cmd
@@ -9,7 +9,7 @@ rem Set the current working directory to the directory where this script is runn
 rem This is important so that all of our relative path references work correctly.
 pushd %~dp0
 
-set CLASSPATH=lib\awakening_2.11-4.19.jar;lib\optparse_2.11-2.2.jar;lib\scala-library-2.11.0.jar;^
+set CLASSPATH=lib\awakening_2.11-4.20.jar;lib\optparse_2.11-2.2.jar;lib\scala-library-2.11.0.jar;^
 lib\scala-parser-combinators_2.11-1.1.2.jar
 java -cp %CLASSPATH%  awakening.LabyrinthAwakening %*
 
