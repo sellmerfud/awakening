9c0da8142464fb5bbc354e8bbb96ec69da0be279
Curt Sellmer <sellmerfud@gmail.com>
2025-01-01 11:26:53 -0600

    refactor: cards 291 - 292

--- src/main/scala/awakening/cards/Card_291.scala
+++ src/main/scala/awakening/cards/Card_291.scala
@@ -10,10 +10,10 @@
 //  / ___ \ V  V / (_| |   <  __/ | | | | | | | (_| |
 // /_/   \_\_/\_/ \__,_|_|\_\___|_| |_|_|_| |_|\__, |
 //                                             |___/
-// An scala implementation of the solo AI for the game 
+// An scala implementation of the solo AI for the game
 // Labyrinth: The Awakening, 2010 - ?, designed by Trevor Bender and
 // published by GMT Games.
-// 
+//
 // Copyright (c) 2010-2017 Curt Sellmer
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -38,10 +38,14 @@
 package awakening.cards
 
 import awakening.LabyrinthAwakening._
+import awakening.JihadistBot
 
 // Card Text:
 // ------------------------------------------------------------------
-//
+// Play in any Hard Schengen country.
+// Blocked by Islamic Maghreb played previously this turn,
+// or if Travel Ban in effect.
+// Place a level 1 Plot there.
 // ------------------------------------------------------------------
 object Card_291 extends Card2(291, "Vehicle-ramming Attacks", Jihadist, 1, NoRemove, NoLapsing, NoAutoTrigger) {
   // Used by the US Bot to determine if the executing the event would alert a plot
@@ -54,21 +58,37 @@ object Card_291 extends Card2(291, "Vehicle-ramming Attacks", Jihadist, 1, NoRem
   override
   def eventRemovesLastCell(): Boolean = false
 
+  def getCandidates() = countryNames(game.getNonMuslims(Schengen).filter(_.isHard))
+
   // Returns true if the printed conditions of the event are satisfied
   override
   def eventConditionsMet(role: Role) = true
+    lapsingEventNotInPlay(IslamicMaghreb) &&
+    globalEventNotInPlay(TravelBan)       &&
+    getCandidates().nonEmpty
 
   // Returns true if the Bot associated with the given role will execute the event
   // on its turn.  This implements the special Bot instructions for the event.
   // When the event is triggered as part of the Human players turn, this is NOT used.
   override
-  def botWillPlayEvent(role: Role): Boolean = true
+  def botWillPlayEvent(role: Role): Boolean = game.availablePlots.contains(Plot1)
 
   // Carry out the event for the given role.
   // forTrigger will be true if the event was triggered during the human player's turn
   // and it associated with the Bot player.
   override
   def executeEvent(role: Role, forTrigger: Boolean): Unit = {
-    ???
+    if (game.availablePlots.contains(Plot1)) {
+      val target = if (isHuman(role))
+        askCountry("Place level 1 Plot in which Hard Schengen country: ", getCandidates())
+      else
+        JihadistBot.plotPriority(getCandidates()).get
+
+      addEventTarget(target)
+      testCountry(target)
+      addAvailablePlotToCountry(target, Plot1, visible = true)
+    }
+    else
+      log("\nThere are no available level 1 plots.  The event has no effect.", Color.Event)
   }
 }
--- src/main/scala/awakening/cards/Card_292.scala
+++ src/main/scala/awakening/cards/Card_292.scala
@@ -10,10 +10,10 @@
 //  / ___ \ V  V / (_| |   <  __/ | | | | | | | (_| |
 // /_/   \_\_/\_/ \__,_|_|\_\___|_| |_|_|_| |_|\__, |
 //                                             |___/
-// An scala implementation of the solo AI for the game 
+// An scala implementation of the solo AI for the game
 // Labyrinth: The Awakening, 2010 - ?, designed by Trevor Bender and
 // published by GMT Games.
-// 
+//
 // Copyright (c) 2010-2017 Curt Sellmer
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -38,10 +38,12 @@
 package awakening.cards
 
 import awakening.LabyrinthAwakening._
+import awakening.JihadistBot
 
 // Card Text:
 // ------------------------------------------------------------------
-//
+// Place a Cadre in up to 3 countries.
+// Test if Unmarked.
 // ------------------------------------------------------------------
 object Card_292 extends Card2(292, "Amaq News Agency", Jihadist, 2, NoRemove, NoLapsing, NoAutoTrigger) {
   // Used by the US Bot to determine if the executing the event would alert a plot
@@ -54,21 +56,65 @@ object Card_292 extends Card2(292, "Amaq News Agency", Jihadist, 2, NoRemove, No
   override
   def eventRemovesLastCell(): Boolean = false
 
+  // Note: The code only allows 1 cadre marker per country.
+  // I have since realized that this is incorrect.
+  def getCandidates() = countryNames(game.countries.filter(!_.hasCadre))
+
+  def getBotCandidates() = {
+    val isCandidate = if (game.botEnhancements)
+      (c: Country) => !c.hasCadre && c.totalCells == 0 && !JihadistBot.isCadreRemovalCandidate(c)
+    else
+      (c: Country) => !c.hasCadre && c.totalCells == 0
+    game.countries.filter(isCandidate)
+  }
+
   // Returns true if the printed conditions of the event are satisfied
   override
-  def eventConditionsMet(role: Role) = true
+  def eventConditionsMet(role: Role) = getCandidates().nonEmpty
 
   // Returns true if the Bot associated with the given role will execute the event
   // on its turn.  This implements the special Bot instructions for the event.
   // When the event is triggered as part of the Human players turn, this is NOT used.
   override
-  def botWillPlayEvent(role: Role): Boolean = true
+  def botWillPlayEvent(role: Role): Boolean = getBotCandidates().nonEmpty
 
   // Carry out the event for the given role.
   // forTrigger will be true if the event was triggered during the human player's turn
   // and it associated with the Bot player.
   override
   def executeEvent(role: Role, forTrigger: Boolean): Unit = {
-    ???
+    val numCadres = if (role == game.humanRole)
+      askInt("Place how many cadres", 1, 3, Some(3))
+    else
+      3
+
+    def nextCadre(num: Int, candidates: List[String]): Unit =
+      if (num <= numCadres && candidates.nonEmpty) {
+        val target = if (role == game.humanRole)
+          askCountry(s"Place ${ordinal(num)} cadre is which country: ", candidates)
+        else
+          JihadistBot.recruitTravelToPriority(candidates).get
+
+        addEventTarget(target)
+        testCountry(target)
+        addCadreToCountry(target)
+        nextCadre(num + 1, candidates filterNot (_ == target))
+      }
+
+    // Used when the event was triggered during US turn
+    val botCriteria = List(
+      new JihadistBot.CriteriaFilter("No Cadre present", _.hasCadre == false),
+      new JihadistBot.CriteriaFilter("No cells present", _.totalCells == 0)
+    )
+
+    val candidates = if (isHuman(role))
+      getCandidates()
+    else if (getBotCandidates().nonEmpty)
+      countryNames(getBotCandidates())
+    else
+      countryNames(JihadistBot.selectCandidates(getBotCandidates(), botCriteria))
+
+
+    nextCadre(1, candidates)
   }
 }
