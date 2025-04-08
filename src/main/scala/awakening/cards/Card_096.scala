742780e4c0c0ec6629483d1b3ddaa5a9dd6f195d
Curt Sellmer <sellmerfud@gmail.com>
2025-04-07 22:51:45 -0500

    fix: enh bot conditions for 96 Danish Cartoons

--- src/main/scala/awakening/cards/Card_096.scala
+++ src/main/scala/awakening/cards/Card_096.scala
@@ -39,6 +39,7 @@ package awakening.cards
 
 import awakening.LabyrinthAwakening._
 import awakening.JihadistBot
+import scala.collection.mutable.ListBuffer
 
 // Card Text:
 // ------------------------------------------------------------------
@@ -66,20 +67,70 @@ object Card_096 extends Card(96, "Danish Cartoons", Unassociated, 1, Remove, NoL
   else
     game.availablePlots
 
+  // Good Muslim, priority to highest Resource, then with troops
+  // Fair Auto-Recruit Muslim, priority to RC
+  // If [Prestige>low]: Poor Muslim with troops, priority to RC, then AID
+  // Fair 3 Resource Muslim
+  // Fair Pakistan
+  // If [Funding <8]: Poor Muslim, priority to Troops, then AID, then Adversary, then Neutral.
+  // Else unplayable
+  def enhBotCandidates: Option[(List[Country], List[JihadistBot.CountryFilter])] = {
+    val buf = new ListBuffer[(List[Country], List[JihadistBot.CountryFilter])]
+    val goodPriorities = List(JihadistBot.HighestResourcePriority, JihadistBot.WithTroopsPriority)
+    val fairAutoRecruitPriorities = List(JihadistBot.HighestResourcePriority, JihadistBot.WithTroopsPriority)
+    val poorWithTroopsPriorities = List(JihadistBot.RegimeChangePriority, JihadistBot.WithAidPriority)
+    val poorPriorities = List(JihadistBot.WithTroopsPriority, JihadistBot.WithAidPriority,
+      JihadistBot.AdversaryPriority, JihadistBot.NeutralPriority)
+
+    val good = game.muslims.filter(_.isGood)
+    val fairAutoRecruit = game.muslims.filter(m => m.isFair && m.autoRecruit)
+    val poorWithTroops = if (game.prestigeLevel != Low)
+      game.muslims.filter(m => m.isPoor && m.totalTroops > 0)
+    else
+      Nil
+    val fair3Resource = game.muslims.filter(m => m.isFair && m.printedResources == 3)
+    val fairPakistan = game.muslims.filter(m => m.isFair && m.name == Pakistan)
+    val poor = if (game.funding < 8)
+      game.muslims.filter(m => m.isPoor)
+    else
+      Nil
+
+
+    buf.append(good -> goodPriorities)
+    buf.append(fairAutoRecruit -> fairAutoRecruitPriorities)
+    buf.append(poorWithTroops -> poorWithTroopsPriorities)
+    buf.append(fair3Resource -> Nil)
+    buf.append(fairPakistan -> Nil)
+    buf.append(poor -> poorPriorities)
+    buf.toList
+      .filter(_._1.nonEmpty)
+      .headOption
+  }
+  
   // Returns true if the Bot associated with the given role will execute the event
   // on its turn.  This implements the special Bot instructions for the event.
   // When the event is triggered as part of the Human players turn, this is NOT used.
+  // Enhanced Jihadist Bot:
+  // Playable if US soft or/and any IR on board and one of the enh Bot candidate lists is non-empty
   override
-  def botWillPlayEvent(role: Role): Boolean =
-    (role == Jihadist && getPlotCandidates.nonEmpty) ||
-    (role == US && getPlotCandidates.isEmpty)
+  def botWillPlayEvent(role: Role): Boolean = role match {
+    case US =>
+      getPlotCandidates.isEmpty
+    case Jihadist if game.botEnhancements =>
+      (game.usPosture == Soft || game.hasMuslim(_.isIslamistRule)) &&
+      getPlotCandidates.nonEmpty &&
+      enhBotCandidates.nonEmpty
+
+    case Jihadist =>
+      getPlotCandidates.nonEmpty
+  }
 
   // Carry out the event for the given role.
   // forTrigger will be true if the event was triggered during the human player's turn
   // and it associated with the Bot player.
   override
   def executeEvent(role: Role): Unit = {
-    log("\nUS side selects Scandinavia's Posture.", Color.Event)
+    log("\nUS selects Scandinavia's Posture.", Color.Event)
     val posture = if (isHuman(US))
       askPosture(Scandinavia)
     else
@@ -90,7 +141,11 @@ object Card_096 extends Card(96, "Danish Cartoons", Unassociated, 1, Remove, NoL
 
     val plotCandidates = getPlotCandidates
     if (plotCandidates.nonEmpty) {
-      log()
+      if (game.numIslamistRule == 0)
+        log(s"\n$Jihadist places any available plot in a Non-Islamist Rule muslim country.", Color.Event)
+      else
+        log(s"\n$Jihadist places an available Plot-1 in a Non-Islamist Rule muslim country.", Color.Event)
+
       val plot = plotCandidates match {
         case p::Nil => p
         case plots if isHuman(Jihadist) => askPlots(plots, 1).head
@@ -99,6 +154,12 @@ object Card_096 extends Card(96, "Danish Cartoons", Unassociated, 1, Remove, NoL
       val candidates = countryNames(game.muslims.filter(m => !m.truce && !m.isIslamistRule))
       val name = if (isHuman(Jihadist))
         askCountry(s"Place $plot in which country: ", candidates)
+      else if (game.botEnhancements) {
+        val (candidates, priorities) = enhBotCandidates.get
+        JihadistBot.topPriority(candidates, priorities)
+          .map(_.name)
+          .get
+      }
       else
         JihadistBot.plotPriority(candidates).get
 
